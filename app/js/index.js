1b3328f803b3c25728ea7afb897f8cb262e4a091
yu tian <fulei7819@163.com>
2016-11-01 20:06:32 +0800

    我的金库样式已完成

--- app/js/index.js
+++ app/js/index.js
@@ -1,9 +1,11 @@
-document.addEventListener("DOMContentLoaded",function(){
-	var mySwiper = new Swiper ('.swiper-container', {
-	    autoplay: 3000,
-	    loop: true,
-	    autoplayDisableOnInteraction : false,
-	    // 如果需要分页器
-	    pagination: '.swiper-pagination',
-	  });        
-});
+;(function(){
+	document.addEventListener("DOMContentLoaded",function(){
+		var mySwiper = new Swiper ('.swiper-container', {
+		    autoplay: 3000,
+		    loop: true,
+		    autoplayDisableOnInteraction : false,
+		    // 如果需要分页器
+		    pagination: '.swiper-pagination',
+		  });        
+	});
+})();
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
@@ -66,26 +65,3 @@ gulp.task('buildhtml',function(){
 
 // 全站编译
 gulp.task('default',['buildSass','jtSass','buildjs','buildhtml']);
-=======
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-
-// 创建task
-// 目的：编辑sass
-gulp.task('buildSass',function(){
-	//查找sass文件
-	gulp.src('./app/sass/*.scss')
-
-		// 把文档流输入出到gulp-sass进行编辑
-		.pipe(sass({outputStyle:'compressed'}))
-
-		// 输出文件
-		.pipe(gulp.dest('./app/css'))
-});
-
-
-gulp.task('jtSass',function(){
-	
-	gulp.watch('./app/sass/*.scss',['buildSass'])
-});
->>>>>>> 484936a95df74885d8f4d2c93366f45ead897227
