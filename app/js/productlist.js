90b7c4b028da43fe0c8df3dca2410052a42e3d90
HCS <2430724495@qq.com>
2016-11-03 18:05:20 +0800

    保存信息

--- app/js/productlist.js
+++ app/js/productlist.js
@@ -18,15 +18,22 @@ var $products_list=$('.products_list');
 			    	 		console.log(item.products)
 			    	 		//遍历生成li
 			    	 		$.each(item.products,function(i,obj){
-			    	 			console.log(obj)
-			    	 			$("<li class='"+item.brand_name+"'><a href='detail.html'><img src='"+obj.product_brand_src1+"' class='product_big'/><div class='title'>"+obj.product_name+"</div><div class='price'><img src='../img/gold.png' /><span class='price_val'>998</span></div></a></li>").appendTo($products_list);
+			    	 			console.log(obj);
+			    	 			$("<li class='"+item.brand_name+"'><a href='detail.html'><img src='"+obj.product_brand_src1+"' class='product_big'/><div class='title'>"+obj.product_name+"</div><div class='price'><img src='../img/gold.png' /><span class='price_val'>"+obj.product_price+"</span></div></a></li>").appendTo($products_list);
 			    	 		});			    	 		
 			    	 	}
 				     });
 			     }
-//			     $products_list.children("li").on("tap",function(){
-//						
-//				 });
+			     $products_list.children("li").on("tap",function(){
+						var brand_name=$(this).attr("class");
+						var product_src1=$(this).find("img").attr("src");
+						var product_name=$(this).find(".title").html();
+						var product_price=$(this).find(".price_val").html();
+						localStorage.setItem("brand_name",brand_name);
+						localStorage.setItem("product_src1",product_src1);
+						localStorage.setItem("product_name",product_name);
+						localStorage.setItem("product_price",product_price);
+				 });
 		    }  
 		});
 		$.ajax();
